e9826a9a40a37f660bb1667ca9655821e6970591
nkastratovic <nkastratovic@gmail.com>
2025-06-20 15:48:26 +0200

    31. Required and Optional Inputs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from "./header/header";
 import { User } from "./user/user";
 
@@ -11,4 +12,5 @@ import { User } from "./user/user";
 })
 export class App {
   protected title = 'first-angular-app';
+  protected users = DUMMY_USERS;
 }
