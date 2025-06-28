c30ec7c9f9fed6cb44710d054a2de6e11debc79d
nkastratovic <nkastratovic@gmail.com>
2025-06-28 06:56:12 +0200

    37. Exercise: Create a Configurable Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,19 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DUMMY_USERS } from './dummy-users';
 import { Header } from "./header/header";
+import { Tasks } from './tasks/tasks';
 import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, User],
+  imports: [RouterOutlet, Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'first-angular-app';
   protected users = DUMMY_USERS;
+  protected selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)!;
+  }
 
   OnSelectUser(id: string) {
-    console.log(`Selected user with id: ${id}`);
+    this.selectedUserId = id;
   }
 }
